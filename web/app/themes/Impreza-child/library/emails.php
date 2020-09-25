948eb3a8cec4757c8781c34cac3ca5b8285999f0
Jeff Miller <jeff@jeffmiller.com.au>
2020-09-25 22:38:35 +1000

    Email template updates

--- web/app/themes/Impreza-child/library/emails.php
+++ web/app/themes/Impreza-child/library/emails.php
@@ -8,7 +8,7 @@ function send_tracking_email($email_subject, $email_content, $header_image ) {
     $content_header = '<div style="text-align: center;">
   <img src="' . get_stylesheet_directory_uri() . '/dist/images/' . $header_image . '" alt="' . $email_subject . '" />
 </div>';
-    $content_footer = '<div style="background-color: #010029; margin: 30px -20px -20px; text-align: center;">
+    $content_footer = '<p style="background-color: #010029; margin: 30px -20px -20px; text-align: center;">
 <table style="width: 100%">
     <tr>
       <td style="padding: 40px;border-bottom: 1px solid #515b99;">
@@ -25,17 +25,17 @@ function send_tracking_email($email_subject, $email_content, $header_image ) {
           <p style="font-size: 14px; color: #515b99; font-weight: bold; margin: 30px auto 0;">Follow Us</p>
           <table style="width: 220px; margin: 0 auto;">
             <tr>
-              <td style="padding: 10px 20px 0;">
+              <td style="padding: 0 20px;">
                 <a href="#">
                   <img src="' . get_stylesheet_directory_uri() . '/dist/images/email-insta.png" alt="Instagram" />
                 </a>
               </td>
-              <td style="padding: 10px 20px 0;">
+              <td style="padding: 0 20px;">
                 <a href="#">
                   <img src="' . get_stylesheet_directory_uri() . '/dist/images/email-facebook.png" alt="Facebook" />
                 </a>
               </td>
-              <td style="padding: 10px 20px 0;">
+              <td style="padding: 0 20px;">
                 <a href="#">
                   <img src="' . get_stylesheet_directory_uri() . '/dist/images/email-linkedin.png" alt="Linked In" />
                 </a>
@@ -45,7 +45,7 @@ function send_tracking_email($email_subject, $email_content, $header_image ) {
         </td>
     </tr>
 </table>
-</div>';
+</p><style>#template_footer{border-top: none !important;}</style>';
 
     $email = get_field( 'email_address' );
     $firstname = get_field( 'first_name' );
@@ -53,7 +53,7 @@ function send_tracking_email($email_subject, $email_content, $header_image ) {
     $subject = get_field( $email_subject, 'option');
     $content = get_field( $email_content, 'option');
 
-    wp_mail( $email, $subject, '<h2 style="text-align: center;">Dear ' . $firstname . ',</h2><br/>' . $content . $content_footer, $headers );
+    wp_mail( $email, $subject, $content_header . '<h2 style="text-align: center;">Dear ' . $firstname . ',</h2><br/>' . $content . $content_footer, $headers );
 }
 
 
