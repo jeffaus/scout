de0af50041b4e8916dcdfd9d325a909e3c19ed69
Jeff Miller <jeff@jeffmiller.com.au>
2020-09-25 18:59:57 +1000

    Add email footer and refactor

--- web/app/themes/Impreza-child/library/emails.php
+++ web/app/themes/Impreza-child/library/emails.php
@@ -3,35 +3,40 @@
 // Tells WP to do something at the do_action mentioned above
 add_action( 'all_admin_notices', 'admin_email_notice');
 
-function send_production_email() {
+
+function send_tracking_email($email_subject, $email_content) {
+    $content_footer = '<div style="background-color: #010029;">
+
+<a href="https://www.akela.supply/" style="display: block;color: #515b99;text-decoration: none;font-weight: bold;font-size: 22px;padding: 40px;border-bottom: 1px solid #515b99;margin: -20px -20px 0">MY ACCOUNT</a>
+
+<a href="https://www.akela.supply/" style="display: block;color: #515b99;text-decoration: none;font-weight: bold;font-size: 22px;padding: 40px;border-bottom: 1px solid #515b99;margin: 0 -20px">CONTACT US</a>
+
+<div style="height: 30px;"></div>
+
+    <a href="#" style="display: inline-block; padding: 10px">
+      <img src="http://track-my-order.akela.supply/app/uploads/2020/09/email-insta.png" />
+    </a>
+
+    <a href="#" style="display: inline-block; padding: 10px">
+      <img src="http://track-my-order.akela.supply/app/uploads/2020/09/email-facebook.png" />
+    </a>
+
+    <a href="#" style="display: inline-block; padding: 10px">
+      <img src="http://track-my-order.akela.supply/app/uploads/2020/09/email-linkedin.png" />
+    </a>
+
+<div style="height: 20px;"></div>
+</div>';
+
     $email = get_field( 'email_address' );
     $firstname = get_field( 'first_name' );
     $headers = 'From: Akela <no-reply@scout.supply>;' . '\r\n';
-    $subject = get_field( 'email_production_subject', 'option');
-    $content = get_field( 'email_production', 'option');
+    $subject = get_field( $email_subject, 'option');
+    $content = get_field( $email_content, 'option');
 
-    wp_mail( $email, $subject, '<h2>Dear ' . $firstname . ',</h2><br/>' . $content, $headers );
+    wp_mail( $email, $subject, '<h2 style="text-align: center;">Dear ' . $firstname . ',</h2><br/>' . $content . $content_footer, $headers );
 }
 
-function send_shipping_email() {
-    $email = get_field( 'email_address' );
-    $firstname = get_field( 'first_name' );
-    $headers = 'From: Akela <no-reply@scout.supply>;' . '\r\n';
-    $subject = get_field( 'email_shipped_subject', 'option');
-    $content = get_field( 'email_shipped', 'option');
-
-    wp_mail( $email, $subject, '<h2>Dear ' . $firstname . ',</h2><br/>' . $content, $headers );
-}
-
-function send_delivery_email() {
-    $email = get_field( 'email_address' );
-    $firstname = get_field( 'first_name' );
-    $headers = 'From: Akela <no-reply@scout.supply>;' . '\r\n';
-    $subject = get_field( 'email_delivered_subject', 'option');
-    $content = get_field( 'email_delivered', 'option');
-
-    wp_mail( $email, $subject, '<h2>Dear ' . $firstname . ',</h2><br/>' . $content, $headers );
-}
 
 // This is the function specified in the "add_action" above
 function admin_email_notice( ) {
@@ -39,15 +44,15 @@ function admin_email_notice( ) {
 
     if(isset($_POST['button1'])) {
         $notice = "Production email sent!";
-        send_production_email();
+        send_tracking_email( 'email_production_subject', 'email_production' );
     }
     if(isset($_POST['button2'])) {
         $notice = "Shipped Email Sent!";
-        send_shipping_email();
+        send_tracking_email( 'email_shipped_subject', 'email_shipped' );
     }
     if(isset($_POST['button3'])) {
         $notice = "Delivered Email Sent!";
-        send_delivery_email();
+        send_tracking_email( 'email_delivered_subject', 'email_delivered' );
     }
 
     if ( $notice ) {
